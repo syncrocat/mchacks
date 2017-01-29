809ccc7d5d1c7c8e236ad684ef21b201dfd5e7f5
paulinebennett <paulinefbennett@Paulines-Macbook-Air.local>
2017-01-29 05:36:13 -0500

    catz and no tool fixed

--- Extension/contentscript.js
+++ Extension/contentscript.js
@@ -1,70 +1,82 @@
 // From http://stackoverflow.com/questions/4445102/google-chrome-extension-highlight-the-div-that-the-mouse-is-hovering-over
 // Unique ID for the className.
-// var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
+var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
 // chrome.runtime.sendMessage({method: 'asdf'});
 
-// Previous dom, that we want to track, so we can remove the previous styling.
-// var prevDOM = null;
-//
-// var mode = "tool-none";
+//Previous dom, that we want to track, so we can remove the previous styling.
+var prevDOM = null;
 
-// Mouse listener for any move event on the current document.
-// document.addEventListener('mousemove', function (e) {
-//   var srcElement = e.srcElement;
-//
-//   // Lets check if our underlying element is a DIV.
-//   if (srcElement.nodeName == 'DIV') {
-//
-//     // For NPE checking, we check safely. We need to remove the class name
-//     // Since we will be styling the new one after.
-//     if (prevDOM != null) {
-//       prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
-//     }
-//
-//     // Add a visited class name to the element. So we can style it.
-//     srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-//
-//     // The current element is now the previous. So we can remove the class
-//     // during the next iteration.
-//     prevDOM = srcElement;
-//   }
-// }, false);
+var mode = "tool-none";
 
-// document.addEventListener("click", function(e) {
-//   if (prevDOM != null) {
-//     if (mode == "tool-none") {
-//
-//     } else if (mode == "tool-set_hidden") {
-//         prevDOM.style.opacity = "0";
-//     } else if (mode == "tool-set_delete"){
-//         prevDOM.hidden = true;
+//Mouse listener for any move event on the current document.
+document.addEventListener('mousemove', function (e) {
+  if (mode == "tool-none") {
+    prevDOM = null;
+  } else {
+    var srcElement = e.srcElement;
+
+    // Lets check if our underlying element is a DIV.
+    if (srcElement.nodeName == 'DIV') {
+
+      // For NPE checking, we check safely. We need to remove the class name
+      // Since we will be styling the new one after.
+      if (prevDOM != null) {
+        prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
+      }
+
+      // Add a visited class name to the element. So we can style it.
+      srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
+
+      // The current element is now the previous. So we can remove the class
+      // during the next iteration.
+      prevDOM = srcElement;
+    }
+  }
+}, false);
+
+document.addEventListener("click", function(e) {
+  if (prevDOM != null) {
+    if (mode == "tool-set_hidden") {
+        prevDOM.style.opacity = "0";
+    } else if (mode == "tool-set_delete"){
+        prevDOM.hidden = true;
 //     } else if (mode == "tool-drag_and_drop") {
 //        prevDOM.ondragstart="event.dataTransfer.setData('text/plain',null)"
 //        prevDOM.draggable="true";
 //     } else if (mode == "tool-color") {
 //         prevDOM.style.color = "purple";
 //     }
-//   }
-// });
+    }else if (mode == "tool-cat"){
+      var cat = document.createElement("DIV");
+      cat.setAttribute("width", prevDOM.offsetWidth);
+      cat.setAttribute("height", prevDOM.offsetHeight);
+      cat.setAttribute("background-color", "red");
+      cat.setAttribute("background-image", "url('https://s-media-cache-ak0.pinimg.com/originals/c2/48/36/c24836d55ec95a86f1bda1b42b2297c0.jpg')");
+      cat.setAttribute("background-size", "cover");
+      prevDOM.parentNode.insertBefore(cat, prevDOM);
+      prevDOM.parentNode.removeChild(prevDOM);
+    }
+  }
+});
 
-// chrome.runtime.onMessage.addListener(function(request) {
-//   if (request.msg == "tool-none") {
-//     mode = "tool-none";
-//   }
-//   else if (request.msg == "tool-set_hidden") {
-//     mode = "tool-set_hidden";
-//   }
-//   else if (request.msg == "tool-set_delete") {
-//     mode = "tool-set_delete";
-//   }
-//   else if (request.msg == "tool-drag_and_drop") {
-//     mode = "tool-drag_and_drop";
-//   }
-//   else if (request.msg == "tool-color") {
-//     mode = "tool-color";
-//   }
-// });
+chrome.runtime.onMessage.addListener(function(request) {
+  if (request.msg == "tool-none") {
+    mode = "tool-none";
+  }
+  else if (request.msg == "tool-set_hidden") {
+    mode = "tool-set_hidden";
+  }
+  else if (request.msg == "tool-set_delete") {
+    mode = "tool-set_delete";
+  }
+  // else if (request.msg == "tool-drag_and_drop") {
+  //   mode = "tool-drag_and_drop";
+  // }
+  else if (request.msg == "tool-cat") {
+    mode = "tool-cat";
+  }
+});
 //
 // document.addEventListener("drag", function( event ) {
 //
